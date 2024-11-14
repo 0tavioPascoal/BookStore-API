ef969698a89d2d6b2f46726c0624567e08761d17
OtavioPascoal <fjoooosa@gmail.com>
2024-11-14 10:52:31 -0300

    Feat: Improving the validation of Author fields

--- src/main/java/com/Tavin/bookstore/infra/dtos/Authors/AuthorsRequestDto.java
+++ src/main/java/com/Tavin/bookstore/infra/dtos/Authors/AuthorsRequestDto.java
@@ -3,16 +3,23 @@ package com.Tavin.bookstore.infra.dtos.Authors;
 import com.Tavin.bookstore.model.AuthorModel;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 import java.util.UUID;
 
 public record AuthorsRequestDto(UUID id,
                                 @NotBlank(message = "required field")
+                                        @Size(max = 100,
+                                        message = "field outside the standard size")
                                 String name,
                                 @NotNull(message = "required field")
+                                        @Past(message = "It cannot be a future date")
                                 LocalDate dateofbirth,
                                 @NotBlank(message = "required field")
+                                        @Size(max = 50, min = 2,
+                                                message = "field outside the standard size")
                                 String nationality) {
 
     public AuthorModel mappedAuthors(){
